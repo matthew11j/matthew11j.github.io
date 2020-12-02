dde9a9c5fad917cece89c4f3d9e885be8e54873d
Matthew Jackson <matthewjackson6118@gmail.com>
2020-12-01 20:17:01 -0500

    Updates

---
+++ precache-manifest.270abe242822f0cfd3e38399fd15ad2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f02fdfdfc4cd5edb3ffcc7d31e8fca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977846f30bfdd9f98915",
+    "url": "/static/css/main.e509fa4d.chunk.css"
+  },
+  {
+    "revision": "904e233805e82be00300",
+    "url": "/static/js/2.fcb7c0a9.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/static/js/2.fcb7c0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977846f30bfdd9f98915",
+    "url": "/static/js/main.fbce9156.chunk.js"
+  },
+  {
+    "revision": "ca87d4764bfbadb807ee",
+    "url": "/static/js/runtime-main.bf0b46b0.js"
+  },
+  {
+    "revision": "c9f0394b2cdaec357292594ff2560c59",
+    "url": "/static/media/Matthew_Jackson_Resume.c9f0394b.pdf"
+  },
+  {
+    "revision": "28735b3a94cfc4bdb92cc55870b4c93f",
+    "url": "/static/media/headshot.28735b3a.png"
+  },
+  {
+    "revision": "04152a91c9953ac14dd7199cf8653e79",
+    "url": "/static/media/me.04152a91.png"
+  }
+]);
